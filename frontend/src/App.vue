6b8df5b22eea3234efa6e93d28de11f2b6ceda1a
André Torquato <andretorquato@alu.ufc.br>
2022-05-29 23:13:26 -0300

    init project

---
+++ frontend/src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts"></script>
+
+<template>
+<div class="poc">Trade POC</div>
+</template>
+
+<style>
+.poc {
+  display: flex;
+  justify-content: center;
+  font-size: 2rem;
+}
+</style>
