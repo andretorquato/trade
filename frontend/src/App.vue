e99a40592e8ef9dc41ce09c0be3ced65e0f23429
André Torquato <andretorquato@alu.ufc.br>
2022-05-31 23:53:46 -0300

    loading coins

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,43 @@
-<script setup lang="ts"></script>
+<script lang="ts">
+import { defineComponent } from "vue";
+import axios from "axios";
+
+export default defineComponent({
+  data() {
+    const marketData: any = null;
+    return { marketData };
+  },
+  methods: {
+    async getMarketData() {
+      const savedData = localStorage.getItem("marketData");
+      if (!savedData) {
+        const response = await axios.get(
+          "http://localhost:5000/cryptocurrency/listings/latest"
+        );
+        if (response.status === 200) {
+          localStorage.setItem("marketData", JSON.stringify(response.data));
+          this.marketData = response.data;
+        }
+      } else {
+        this.marketData = JSON.parse(savedData);
+      }
+    },
+  },
+  mounted() {
+    this.getMarketData();
+  },
+});
+</script>
 
 <template>
-<div class="poc">Trade POC</div>
+  <div v-if="marketData">
+    <div v-for="coin of marketData.data" :key="coin.id">
+      <p class="coin-name">
+        {{ coin.name }} - price: $ {{ coin.quote.USD.price.toFixed(3) }}
+      </p>
+    </div>
+  </div>
+  <div v-else> error load </div>
 </template>
 
 <style>
