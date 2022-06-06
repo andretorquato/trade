31fe67f65c074bc266832e297ff6f9e5bc67d9b1
André Torquato <andretorquato@alu.ufc.br>
2022-06-05 23:06:50 -0300

    connect with mongo

--- backend/main.go
+++ backend/main.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"trade/configs"
+
 	"github.com/gofiber/fiber/v2"
 )
 
 func main() {
 	app := fiber.New()
+
+	// run database
+	configs.ConnectDB()
 
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.JSON(&fiber.Map{"data": "Init Trader backend"})
