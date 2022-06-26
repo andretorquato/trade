a3fee94fed4b1856a92234da9d135600cd38942d
André Torquato <andretorquato@alu.ufc.br>
2022-06-25 23:49:53 -0300

    add to route

--- backend/routes/market_route.go
+++ backend/routes/market_route.go
@@ -1,10 +1,16 @@
 package routes
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"trade/controllers"
+
+	"github.com/gofiber/fiber/v2"
+)
 
 func UserRoute(app *fiber.App) {
 
 	app.Get("/", func(c *fiber.Ctx) error {
-		return c.JSON(&fiber.Map{"data": "Init Trader backend"})
+		return c.JSON(&fiber.Map{"data": "online"})
 	})
+
+	app.Post("/new-log", controllers.CreateMarketData)
 }
