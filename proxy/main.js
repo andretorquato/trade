d5bcca56d0bc640b9c0d14d2023c71b99f245853
André Torquato <andretorquato@alu.ufc.br>
2022-07-01 20:43:47 -0300

    fix save data

--- proxy/main.js
+++ proxy/main.js
@@ -1,6 +1,7 @@
 // Thanks Blitz
 // https://medium.com/@theBliz/why-ive-built-a-coinmarketcap-proxy-e06c898b5765
 const express = require("express");
+var bodyParser = require('body-parser')
 const cors = require("cors");
 const morgan = require("morgan");
 const axios = require("axios");
@@ -8,10 +9,28 @@ const axios = require("axios");
 require("dotenv").config();
 
 const app = express();
+const router = express.Router();
 
 app.use(morgan("tiny"));
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 app.use(cors());
+app.use(router);
 
+router.post("/trade-api/*", (req, res) => {
+  let url = `http://localhost:6000/new-market-data`;
+  axios
+    .post(url, {
+      data: JSON.stringify(req.body.data)
+    })
+    .then((response) => {
+      res.send(response.data);
+    })
+    .catch((err) => {
+      console.log(err.response.data);
+      res.send(err.response.data);
+    });
+});
 app.get("/coinmarketcap", (req, res) => {
   let url = `https://pro-api.coinmarketcap.com/v1${req.originalUrl}`;
   axios
@@ -24,21 +43,11 @@ app.get("/coinmarketcap", (req, res) => {
       res.send(err.response.data);
     });
 });
+// app.get("/*", (req, res) => {
+//   console.log(req);
+//   res.send({status: "ok"})
+// });
 
-app.get("/trade-api", (req, res) => {
-  // let url = `http://localhost:6000${req.originalUrl}`;
-  let url = `http://localhost:6000/new-market-data`;
-  axios
-    .get(url)
-    .then((response) => {
-      res.send(response.data);
-    })
-    .catch((err) => {
-      console.log(err.response.data);
-      res.send(err.response.data);
-    });
-    
-});
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log("Listening on port ", port);
